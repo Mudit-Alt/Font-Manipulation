10c478413c95af4c02b8379233c863cfd9f4d5b2
Mudit_Chandak7C <96127224+Mudit-Alt@users.noreply.github.com>
2022-08-26 17:54:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+WristR = 0
+WristL = 0
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    video = createCapture(VIDEO);
+    video.size(500, 500)
+    canvas.position(600, 100)
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Good Job! Pose Net is working !')
+}
+
+function draw() {
+    background("grey")
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        WristR = results[0].pose.rightWrist.x;
+        WristL = results[0].pose.leftWrist.x;
+        console.log("Right Wrist Is On " + WristR + "Left Wrist Is On " + WristL);
+
+    }
+}
